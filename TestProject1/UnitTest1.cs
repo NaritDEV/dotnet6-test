abf8eb53ab7792d710d33b8d79c4d4f8d2269eee
NaritDEV <81557678+NaritDEV@users.noreply.github.com>
2023-10-03 16:06:25 +0700

    update

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace TestProject1
         [Fact]
         public void Test1()
         {
-            Console.WriteLine("Demo Test Case");
+            Console.WriteLine("Demo Test Case 2");
         }
     }
 }
